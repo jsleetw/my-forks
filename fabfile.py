2382463f3c37b00fbaf1b634ac9a8d693de7b7ff
jsleetw <ail529@gmail.com>
2011-12-15 12:42:47 +0800

    add get_my_fork auto get from github api

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,32 @@
 from fabric.api import run, env
 from fabric.context_managers import settings, cd
-
-my_forks = {
-        "django": {"origin": "git@github.com:jsleetw/django.git", "upstream": "git://github.com/django/django.git"},
-        "ThinkUp": {"origin": "git@github.com:jsleetw/ThinkUp.git", "upstream": "git://github.com/ginatrapani/ThinkUp.git"},
-        "aptsync": {"origin": "git@github.com:jsleetw/aptsync.git", "upstream": "git://github.com/t9md/aptsync.git"},
-        "pfff": {"origin": "git@github.com:jsleetw/pfff.git", "upstream": "git://github.com/facebook/pfff.git"},
-        "tomahawk": {"origin": "git@github.com:jsleetw/tomahawk.git", "upstream": "git://github.com/oinume/tomahawk.git"},
-        "gitflow": {"origin": "git@github.com:jsleetw/gitflow.git", "upstream": "git://github.com/nvie/gitflow.git", "branch": "develop"},
-        "git-extras": {"origin": "git@github.com:jsleetw/git-extras.git", "upstream": "git://github.com/visionmedia/git-extras.git"},
-        "fabric": {"origin": "git@github.com:jsleetw/fabric.git", "upstream": "git://github.com/fabric/fabric.git"},
-        "ssh": {"origin": "git@github.com:jsleetw/ssh.git", "upstream": "git://github.com/bitprophet/ssh.git"},
-        "python-mode": {"origin": "git@github.com:jsleetw/python-mode.git", "upstream": "git://github.com/klen/python-mode.git"},
-        "oh-my-zsh": {"origin": "git@github.com:jsleetw/oh-my-zsh.git", "upstream": "git://github.com/robbyrussell/oh-my-zsh.git"},
-        "django-startproject": {"origin": "git@github.com:jsleetw/django-startproject.git", "upstream": "git://github.com/lincolnloop/django-startproject.git"},
-        "git-flow-completion": {"origin": "git@github.com:jsleetw/git-flow-completion.git", "upstream": "git://github.com/bobthecow/git-flow-completion.git"},
-        "vim_bridge": {"origin": "git@github.com:jsleetw/vim_bridge.git", "upstream": "git://github.com/nvie/vim_bridge.git"},
-        }
+from github2.client import Github
 
 code_dir = '~/test_my_forks/'
 env.hosts = ["localhost"]
 
 
+def get_my_fork():
+    user_name = raw_input("enter your user name : ")
+    github = Github()
+    repos = github.repos.list(user_name)
+    my_forks = {}
+    for element in repos:
+        project = str(element).replace("<Repository:", "").replace(">", "").strip()
+        repo = github.repos.show(project)
+        project_pure_name = project.replace(user_name, "").replace("/", "").strip()
+        if repo.fork:
+            my_forks[project_pure_name] = {}
+            my_forks[project_pure_name]["origin"] = "git@github.com:%s.git" % project
+            my_forks[project_pure_name]["upstream"] = "git@github.com:%s.git" % str(repo.parent)
+            if repo.master_branch:
+                my_forks[project_pure_name]["branch"] = str(repo.master_branch)
+
+    return my_forks
+
+
 def update():
+    my_forks = get_my_fork()
     for element in my_forks.keys():
         project = element
         origin = my_forks[element]["origin"]
